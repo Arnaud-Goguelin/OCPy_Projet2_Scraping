b1a80efaee7831514879b7495ab506b7d4c762e8
Arnaud-Goguelin <arnaud.goguelin@gmail.com>
2024-06-20 15:15:49 +0200

    feat (get_book_infos): now get review rating from soup

--- lib/get_book_infos.py
+++ lib/get_book_infos.py
@@ -2,9 +2,14 @@ from lib.parse_url import get_url_base
 def get_book_infos(soup, book, url):
     # get book's title
     book['title'] = soup.h1.string
+    
+    # get category
+    all_links = soup.find_all('a')
+    book['category'] = all_links[3].get_text()
 
     # get book's description : 
-    # use meta tag because it is unique is the page and the same as the descritpion in <p> tag
+    # use meta tag because it is unique is the page 
+    # and the same as the descritpion in <p> tag
     meta_description = soup.find('meta', attrs={'name': 'description'})
     description = meta_description['content'].strip()
     book['description'] = description
@@ -18,16 +23,29 @@ def get_book_infos(soup, book, url):
             if th[0].get_text() == 'Product Type' or th[0].get_text() =='Number of reviews':
                 continue
             book[th[0].get_text()] = td[0].get_text()
-    
+
+    # get review raiting
+    ratings = {
+    'One': 1,
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5
+    }
+    all_text = soup.find_all('p', class_='star-rating')
+    current_book_ratings = all_text[0]['class'][1]
+    rating = ratings[current_book_ratings]
+    book['review_rating'] = f'{rating}/5'
+  
     # get the book's imagine (the first in all the img tag)
     all_img = soup.find_all('img')
     current_book_img = all_img[0]
-    # get the image url, remove useless characters ('../') and concatenate it with the basic url of the website
+
+    # get the image url, remove useless characters ('../') 
+    # and concatenate it with the basic url of the website
     # in order to get a valide url to use
     current_book_img_url = current_book_img['src'].lstrip('../')
     base_url = get_url_base(url)
     book['image_url'] = f'{base_url}/{current_book_img_url}'
 
     return book
-
-    
--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-"""
-extraire les informations suivantes :
-● product_page_url OK
-● universal_ product_code (upc) OK
-● title OK
-● price_including_tax OK
-● price_excluding_tax OK
-● number_available OK
-● product_description 
-● category 
-● review_rating
-● image_url
-"""
 from bs4 import BeautifulSoup
 from lib.extract_data import extract_data
 from lib.get_book_infos import get_book_infos
@@ -20,11 +7,11 @@ url = "http://books.toscrape.com/catalogue/the-grand-design_405/index.html"
 if __name__ == '__main__':
 
     page = extract_data(url)
-    #print(page)
     soup = BeautifulSoup(page, "html.parser")
 
     book = {}
-    get_book_infos(soup, book)
+    book['product_page_url'] = url
+    get_book_infos(soup, book, url)
     print(book)
 
 # Attention à respecter les règles de scraping

db915b72326c7218bf60726a04d83fff14fb4dc4
Arnaud-Goguelin <arnaud.goguelin@gmail.com>
2024-06-20 16:33:03 +0200

    style (get_book_infos.py): change keys names

--- lib/get_book_infos.py
+++ lib/get_book_infos.py
@@ -4,20 +4,19 @@ def get_book_infos(page, book, url):
 
     soup = BeautifulSoup(page, "html.parser")
 
-
     # get book's title
-    book['title'] = soup.h1.string
+    book['Title'] = soup.h1.string
     
     # get category
     all_links = soup.find_all('a')
-    book['category'] = all_links[3].string
+    book['Category'] = all_links[3].string
 
     # get book's description : 
     # use meta tag because it is unique is the page 
     # and the same as the descritpion in <p> tag
     meta_description = soup.find('meta', attrs={'name': 'description'})
     description = meta_description['content'].strip()
-    book['description'] = description
+    book['Description'] = description
 
     # get more infos (UPC, Prices, Tax, Availability) on the book, 
     # exclude useless infos
@@ -25,7 +24,7 @@ def get_book_infos(page, book, url):
     all_th = table.find_all('th')
     all_td = table.find_all('td')
     for (th, td) in zip(all_th, all_td):
-        if th.string == 'Product Type' or th.string =='Number of reviews':
+        if th.string == 'Product Type' or th.string =='Number of reviews' or th.string =='Tax':
             continue
         book[th.string] = td.string
 
@@ -40,7 +39,7 @@ def get_book_infos(page, book, url):
     all_text = soup.find_all('p', class_='star-rating')
     current_book_ratings = all_text[0]['class'][1]
     rating = ratings[current_book_ratings]
-    book['review_rating'] = f'{rating}/5'
+    book['Rating'] = f'{rating}/5'
   
     # get the book's imagine (the first in all the img tag)
     all_img = soup.find_all('img')
@@ -51,6 +50,6 @@ def get_book_infos(page, book, url):
     # in order to get a valide url to use
     current_book_img_url = current_book_img['src'].lstrip('../')
     base_url = get_url_base(url)
-    book['image_url'] = f'{base_url}/{current_book_img_url}'
+    book['Image_url'] = f'{base_url}/{current_book_img_url}'
 
     return book

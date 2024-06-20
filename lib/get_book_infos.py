4d0e6fdd4b3efb472a3d74990da0da7aa3a632ba
Arnaud-Goguelin <arnaud.goguelin@gmail.com>
2024-06-20 13:39:00 +0200

    feat (get_book_info): remove useless infos from book

--- lib/get_book_infos.py
+++ lib/get_book_infos.py
@@ -1,10 +1,13 @@
 def get_book_infos(soup, book):
-    book['title'] = soup.title.string
+    book['title'] = soup.title.string.strip()
 
     for table in soup.find_all('table'):
         for tr in table.find_all('tr'):
             th = tr.find_all('th')
             td = tr.find_all('td')
+            #print(th[0].get_text)
+            if th[0].get_text() == 'Product Type' or th[0].get_text() =='Number of reviews':
+                continue
             book[th[0].get_text()] = td[0].get_text()
     
     return book

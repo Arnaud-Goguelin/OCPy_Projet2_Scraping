2dbd350525a08334dd9365da030f1467691b0b34
Arnaud-Goguelin <arnaud.goguelin@gmail.com>
2024-06-28 15:30:07 +0200

    feat (get_html.py): add 1 sec delay between to request

--- lib/get_html.py
+++ lib/get_html.py
@@ -1,10 +1,11 @@
+import time
 import requests
 
 
 def get_html(url):
-
+    # wait 1 sec before make HTTP request in order to prevent server's limiter action
+    time.sleep(1)
+    # make HTTP request
     response = requests.get(url)
-
     html = response.content
-
     return html

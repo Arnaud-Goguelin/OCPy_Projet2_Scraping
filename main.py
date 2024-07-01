cb246f38639a1ed31a7d20b7cbf52eea7bd79562
Arnaud-Goguelin <arnaud.goguelin@gmail.com>
2024-07-01 23:08:13 +0200

    style(main.py): add feed back in console while program is executed

--- main.py
+++ main.py
@@ -20,26 +20,34 @@ website_url = "http://books.toscrape.com/"
 if __name__ == "__main__":
 
     # scrap one book
-    print(f"one book scrappring begins at {get_time()}")
+    starting_time = get_time()
+    print(f"one book scrappring begins at {starting_time}")
     book_page = get_html(book_url)
 
     book = get_book_data(book_page, book_url)
     book["Book_page_url"] = book_url
     export_one_book(book)
-    print(f"one book scrappring ends at {get_time()}\n\n")
+    ending_time = get_time()
+    print(f"one book scrappring ends at {ending_time}\nIt took {ending_time - starting_time}\n\n")
 
-    # scrap all books in one category
-    print(f"one category scrapping begins at {get_time()}")
+    # # scrap all books in one category
+    starting_time = get_time()
+    print(f"one category scrappring begins at {starting_time}")
+
     category_page = get_html(category_url)
 
     books_from_category = get_category_data(category_page, category_url)
     export_category_books(books_from_category)
-    print(f"one category scrapping ends at {get_time()}\n\n")
+    ending_time = get_time()
+    print(f"one category scrappring ends at {ending_time}\nIt took {ending_time - starting_time}\n\n")
 
-    # scrap all books from website
-    print(f"website scrapping begins at {get_time()}")
+    # # scrap all books from website
+    starting_time = get_time()
+    print(f"website scrappring begins at {starting_time}")
+
     landing_page = get_html(website_url)
 
     books_from_website = get_website_data(landing_page, website_url)
     export_website_books(books_from_website)
-    print(f"website scrapping ends at {get_time()}")
+    ending_time = get_time()
+    print(f"website scrappring ends at {ending_time}\nIt took {ending_time - starting_time}\n\n")
